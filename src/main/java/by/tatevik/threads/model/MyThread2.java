083459596c1531478790bc9cc56a2c2d4c3a35ad
Tatevik777 <67191920+Tatevik777@users.noreply.github.com>
2024-12-21 00:09:09 +0300

    update

--- src/main/java/by/tatevik/threads/model/MyThread2.java
+++ src/main/java/by/tatevik/threads/model/MyThread2.java
@@ -18,4 +18,34 @@ public class MyThread2 extends Thread{
             }
         }
     }
-}
+    public static void startThread(){
+        MyThread2 thread1 = new MyThread2("Поток 1");
+        MyThread2 thread2 = new MyThread2("Поток 2");
+
+        thread1.start();  // Запускаем Поток 1
+        try {
+            Thread.sleep(100); // Небольшая задержка перед запуском Потока 2
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        thread2.start();  // Запускаем Поток 2
+
+        // Ждем завершения Потока 2
+        try {
+            thread2.join(); // Ждем, пока Поток 2 завершится
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // Ждем завершения Потока 1
+        try {
+            thread1.join(); // Ждем, пока Поток 1 завершится
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Оба потока завершены.");
+
+    }
+    }
+
